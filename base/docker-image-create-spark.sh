8ff2585695bf0d8c102bb26977a5d4acd5492f98
Rogerio Matte Machado <rogermm@gmail.com>
2021-11-03 15:19:54 -0300

    Fix for making official spark distribuition

---
+++ base/docker-image-create-spark.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+#
+# -n                    Build docker image with --no-cache
+# -m                    Use minikube's Docker daemon.
+#
+
+HADOOP_VERSION=3.3.1
+
+cd dependencies
+mvn clean install dependency:copy-dependencies -Dhadoop.version=$HADOOP_VERSION
+
+cd ../../spark
+
+# shellcheck disable=SC1101
+./dev/make-distribution.sh --name custom-spark --pip \
+    -Phive -Phive-thriftserver -Pkubernetes -Dhadoop.version=$HADOOP_VERSION
+
+if [ -d ./dist ]; then
+  cd dist
+
+  SPARK_HOME=`pwd`
+
+  #14-jre-slim   14.0-jdk-slim-buster
+  TAG=11.0-jdk-slim-buster
+  ./bin/docker-image-tool.sh "$@" \
+                           -t $TAG \
+                           -p kubernetes/dockerfiles/spark/bindings/python/Dockerfile \
+                           -b java_image_tag=$TAG \
+                           build
+
+fi    
