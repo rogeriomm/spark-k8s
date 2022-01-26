3fc8143634ea17df5128d1840be95ac5ca2bb80d
Rogerio Matte Machado <rogermm@gmail.com>
2022-01-26 15:50:36 -0300

    Refactoring

--- base/spark-create-image.sh
+++ base/spark-create-image.sh
@@ -9,22 +9,31 @@ set -x
 set -e
 
 HADOOP_VERSION=3.3.1
+SPARK_VERSION=3.1.2
+SPARK_MAJOR_VERSION=$(awk -F '.' '{print $1"."$2}' <<< $SPARK_VERSION)
+DELTA_VERSION=1.0.0
+
+TAG=11.0-jdk-slim-bullseye
+
+unset SPARK_HOME
 
 cd dependencies
-mvn clean install dependency:copy-dependencies -Dhadoop.version=$HADOOP_VERSION
+mvn clean install dependency:copy-dependencies -Dhadoop.version=$HADOOP_VERSION -Ddelta.version=${DELTA_VERSION}
 
-cd ../../spark
+cd "../spark-$SPARK_MAJOR_VERSION"
+git checkout "v$SPARK_VERSION"
 
 # shellcheck disable=SC1101
-./dev/make-distribution.sh --name custom-spark --pip -DskipTests=true\
-    -Phive -Phive-thriftserver -Pkubernetes -Dhadoop.version=$HADOOP_VERSION
+#./dev/make-distribution.sh --name custom-spark --pip -DskipTests=true\
+#    -Phive -Phive-thriftserver -Pkubernetes -Dhadoop.version=$HADOOP_VERSION
 
-if [ -d ./dist ]; then
-  cd dist
+export SPARK_HOME="$(pwd)/dist"
 
-  TAG=11.0-jdk-slim-bullseye
+if [ -d "$SPARK_HOME" ]; then
+  cd "$SPARK_HOME"
+
   ./bin/docker-image-tool.sh "$@" \
-                           -t $TAG \
+                           -t "$TAG-$SPARK_VERSION" \
                            -p kubernetes/dockerfiles/spark/bindings/python/Dockerfile \
                            -b java_image_tag=$TAG \
                            build
--- build.mk
+++ build.mk
@@ -1,4 +1,10 @@
-Prj([Docker(name="base",        prefix="spark"),
-     Docker(name="base-python", prefix="spark"),
-     Docker(name="jupyter",     prefix="spark")],
+Prj([
+     Docker(name="base",        version="3.1.2", prefix="spark", parm={"SPARK_VERSION": "3.1.2"}),
+     Docker(name="base-python", version="3.1.2", prefix="spark", parm={"SPARK_VERSION": "3.1.2"}),
+     Docker(name="jupyter",     version="3.1.2", prefix="spark", parm={"SPARK_VERSION": "3.1.2"}),
+
+     Docker(name="base",        version="3.2.0", prefix="spark", parm={"SPARK_VERSION": "3.2.0"}),
+     Docker(name="base-python", version="3.2.0", prefix="spark", parm={"SPARK_VERSION": "3.2.0"}),
+     Docker(name="jupyter",     version="3.2.0", prefix="spark", parm={"SPARK_VERSION": "3.2.0"})
+     ],
      "docker-spark")
